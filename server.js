126265e7c49348fce02ff67181de99369b020b02
usamasarfraz <usamasarfraz822@gmail.com>
2019-05-11 11:40:23 +0500

    add port for server.

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const db = require('./db/db');
 const loginRoute = require('./routes/userRoutes/loginRoute');
 const registerRoute = require('./routes/userRoutes/registerRoute');
 const forgetPasswordRoute = require('./routes/userRoutes/forgetPasswordRoute');
+let port = process.env.PORT || 6249;
 
+const http = require('http').Server(server);
 server.use(bodyParser.json());
 
 server.use('/user/login',loginRoute);
 server.use('/user/register',registerRoute);
 server.use('/user/forgetpassword',forgetPasswordRoute);
-server.listen(6249,()=>{
+http.listen(6249,()=>{
     console.log('Server is up on 6249.');
 });

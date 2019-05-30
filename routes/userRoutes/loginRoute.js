92037a01fc8059b1fac872579abb6a09f147e4f9
usamasarfraz <usamasarfraz822@gmail.com>
2019-05-30 18:34:10 +0500

    login response with data

--- routes/userRoutes/loginRoute.js
+++ routes/userRoutes/loginRoute.js
@@ -5,7 +5,7 @@ const User = require('../../models/userModel/userModel');
 user.post('/login_user',(req,res)=>{
     User.findOne({$and: [{email: req.body.email}, {password: req.body.password}]},function(err,data){
         if(data){
-            res.json({dataFound:true});
+            res.json({dataFound:true,data: data});
         }
         else{
             res.json({dataFound:false});

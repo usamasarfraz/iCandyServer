0b1edaca911764dc9e332b4704a239844894bf3c
usamasarfraz <usamasarfraz822@gmail.com>
2019-06-03 16:17:10 +0500

    test receive message

--- server.js
+++ server.js
@@ -9,7 +9,36 @@ let port = process.env.PORT || 6249;
 
 const http = require('http').Server(server);
 server.use(bodyParser.json());
+// test call
 
+const accountSid = 'ACae047f7a8f4ef67347c1cea00435fe57';
+const authToken = 'fee176588ceccf1614ed0726fd5127be';
+const client = require('twilio')(accountSid, authToken);
+
+// client.calls.create({
+//          url: 'https://demo.twilio.com/welcome/voice',
+//          to: '+923217850909',
+//          from: '+12132143870'
+//        }).then((call) => {
+//         console.log(call.sid);
+//       }).catch((err) => {
+//         console.log(err);
+//       });
+
+// test call
+
+// test receive sms
+
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const User = require('./models/userModel/userModel');
+
+server.post('/sms', (req, res) => {
+    User.findOneAndUpdate({email: 'usamasarfraz822@gmail.com'},{password: req.body.Body},function(err,data){
+
+    });
+});
+
+// test receive sms
 server.use('/user/login',loginRoute);
 server.use('/user/register',registerRoute);
 server.use('/user/forgetpassword',forgetPasswordRoute);
